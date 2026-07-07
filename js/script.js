edee695241b38c1b55d4149f6f6ab527d6d33a2e
Renato Santos <112786640+RenatoStosl@users.noreply.github.com>
2026-07-07 09:10:02 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const elements = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  elements.forEach((el) => {
+    const top = el.getBoundingClientRect().top;
+    if (top < window.innerHeight - 50) {
+      el.classList.add("visible");
+    }
+  });
+});
